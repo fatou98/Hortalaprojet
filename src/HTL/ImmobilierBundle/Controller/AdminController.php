415512b50fa5d4d8a0ff7906a1d06f2ad8456b8c
fallougueye <34451932+fallougueye@users.noreply.github.com>
2018-03-01 19:15:01 +0000

    Add files via upload

--- src/HTL/ImmobilierBundle/Controller/AdminController.php
+++ src/HTL/ImmobilierBundle/Controller/AdminController.php
@@ -3,67 +3,41 @@
 namespace HTL\ImmobilierBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use HTL\ImmobilierBundle\Entity\Bien;
+use HTL\ImmobilierBundle\bien\BienType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-use HTL\ImmobilierBundle\Entity\Bien;
-    use HTL\ImmobilierBundle\Entity\Client;
-    use HTL\ImmobilierBundle\Form\ClientType;
-    use HTL\ImmobilierBundle\Entity\Typebien;
-    use HTL\ImmobilierBundle\Entity\Reservation;
-    use HTL\ImmobilierBundle\Entity\Localite;
-    use Symfony\Component\HttpFoundation\Response;
-    use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
-    use Knp\Bundle\PaginatorBundle\Helper\Processeur;
-    use HTL\ImmobilierBundle\Form\BienType;
-
 
 
 class AdminController extends Controller
 {
-    /**
-     * @Route("/listbien")
-     */
-    public function listBienAction()
+    public function adminAction(Request $request)
+
     {
-        $em = $this->getDoctrine()->getManager();
-        $bien= $em->getRepository(Bien::class)->FindAll();
-        
-        return $this->render('HTLImmobilierBundle:Admin:list_bien.html.twig', array(
-            'biens' => $bien,
+    
+            $bien = new Bien();
+            if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($bien);
+            $em->flush();
 
-            // ...
-        ));
-    }
-    public function editAction(Request $request, $id){
-        $bien=$this->getDoctrine()
-        ->getManager()
-        ->getRepository('HTLImmobilierBundle:Bien')
-        ->find($id);
-        $formBien= $this->createForm(BienType::class,$bien);
-        if ($request->isMethod('POST')) {
-            $formBien->HandleRequest($request);
-            if ($formBien->isValid()) {
-                $this->getDoctrine()
-                     ->getManager()
-                     ->flush();
-
-            }
+            return new Response('Bien ajouté !');
         }
-        return $this->render('HTLImmobilierBundle:Admin:edit.html.twig',
-         array("formulaire"=>$formBien->createView())
-        );
+    }
+        return $this->render('HTLImmobilierBundle:Front:admin.html.twig', array(
+           ));
+        
+    }
+    //$repository=$this->getDoctrine()->getManager()->getRepository('HTLImmobilierBundle:Client');
+           //if($request->isMethod('post')){
+              
+            //$client=$repository->findBy(["email"=>$_POST['login'],"password"=>$_POST['passord']]);
+            //return $this->redirectToRoute('searchprix');
 
-    }
-    public function supprimerAction(Bien $bien){
-        $em=$this->getDoctrine()->getManager();
-        $em->remove($bien);
-//        $em->persist($bien);
-        $em->flush();
-        return $this->redirectToRoute('list');
-    }
-  
-    public function adminAction(){
-        return $this->render('HTLImmobilierBundle:Admin:admin.html.twig');
- 
-    }
+        
+        //}
+        //return $this->render('HTLImmobilierBundle:user:index
+        
 }
